7886befe6c8b322c88e5eea8abc34b1a335f799a
github username <josephspinelli@yahoo.com>
2019-09-05 22:17:41 -0500

    done

---
+++ loop-and-display-jsx/src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+/**
+ * Our data comes from users-data.js
+ * -----------------------------
+ */
+import users from "./users-data";
+
+/**
+ * Our React component where we display data
+ * -----------------------------
+ */
+function App() {
+  return (
+    <div className="App">
+      <div className="page-deets">
+        <h2>Loop Over an Array of Data</h2>
+      </div>
+
+      {/* Iterate over imported array in userData */}
+      <div className="users">
+        {users.map((user, index) => (
+          <div key={index}>
+            <h3>{user.name}</h3>
+            <p>{user.location}</p>
+            <p>{user.car}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
